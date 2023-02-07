f5777d79efad20960deb16c1c382efe16a75d797
Konate Mamadou <120653410+MamadouKernel@users.noreply.github.com>
2023-02-07 19:11:55 +0000

    Add files via upload

---
+++ src/Exercice1/Cercle.java
@@ -0,0 +1,29 @@
+package Exercice1;
+
+public class Cercle {
+    private double rayon;
+
+    public Cercle() {
+        super();
+    }
+
+    public Cercle(double rayon) {
+        this.rayon = rayon;
+    }
+
+    public double getRayon() {
+        return rayon;
+    }
+
+    public void setRayon(double rayon) {
+        this.rayon = rayon;
+    }
+
+    public double airCercle(){
+        return Math.PI * Math.pow(rayon,2);
+    }
+
+    public double perimetreCercle(){
+        return Math.PI * 2 * rayon;
+    }
+}
